cad2783f0bea6b9a9edb6ed91a99b5d2f17fa947
Joseph Kinsey <kinzoe66@gmail.com>
2026-03-06 11:32:53 -1000

    Add deterministic sequence planner

---
+++ src/sequence/sequence_planner.py
@@ -0,0 +1,14 @@
+class SequencePlanner:
+
+    def generate(self, task):
+        """
+        Generate deterministic ordered sequence.
+        """
+
+        sequence = {
+            "sequence_id": "generated_sequence",
+            "steps": [],
+            "expected_outcome": None
+        }
+
+        return sequence
